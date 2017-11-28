3577a25ff6b640e08af37842f63b696e34674bfa
Seth <seth@eventespresso.com>
2017-11-28 06:30:01 -0700

    Remove the extra hex2rgba function

--- beaver-builder-modules/ticket-selector/includes/functions.php
+++ beaver-builder-modules/ticket-selector/includes/functions.php
@@ -18,25 +18,3 @@ function espresso_module_event_titles() {
 
     return $options;
 }
-
-
-/*
- * Hex to Rgba
- */
-function espresso_hex2rgba( $hex, $opacity )
-{
-    $hex = str_replace( '#', '', $hex );
-
-    if ( strlen($hex) == 3 ) {
-        $r = hexdec(substr($hex,0,1).substr($hex,0,1));
-        $g = hexdec(substr($hex,1,1).substr($hex,1,1));
-        $b = hexdec(substr($hex,2,1).substr($hex,2,1));
-    } else {
-        $r = hexdec(substr($hex,0,2));
-        $g = hexdec(substr($hex,2,2));
-        $b = hexdec(substr($hex,4,2));
-    }
-    $rgba = array($r, $g, $b, $opacity);
-
-    return 'rgba(' . implode(', ', $rgba) . ')';
-}
